c1b6fc86ed948c5f275e383e0d7a85e55823b752
Toby <toby.lawrance@gmail.com>
2020-12-11 15:55:51 +0000

    Day11

---
+++ Day10/Program.fs
@@ -0,0 +1,85 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+open System.Reflection.Metadata
+
+let fac x =
+    let rec facR i acc =
+        match i with
+        | 0L | 1L -> acc
+        | _ -> facR (i - 1L) (acc * i)
+    facR x 1L
+
+let passThroughPrint msg value =
+    printfn "%s %A" msg value
+    value
+    
+let partA adapters =
+    let withExtra = List.append [0L;(List.max adapters) + 3L] adapters
+    let jolts = List.sort withExtra
+                |> List.windowed 2
+                |> List.map (fun (l:int64 list) -> (List.last l) - (List.head l))
+                |> List.groupBy id
+                |> List.map (fun (i,il) -> (i,List.length il))
+    
+    printfn "Jolt differences:  %A" jolts
+    
+    if List.exists (fun (i:int64,_) -> i > 3L) jolts then
+        failwith "problemo"
+    
+    (snd (List.find (fun (i:int64,_) -> i = 1L) jolts)) * (snd (List.find (fun (i:int64,_) -> i = 3L) jolts))
+
+let groupsPassingPredicate pred l =
+    let rec grouping (acc : int list list) = function
+        | [] -> acc
+        | x -> let firstGroup = List.takeWhile pred x
+               let firstSkip = if List.length firstGroup > 0 then
+                                    List.skip (List.length firstGroup) x
+                               else
+                                   List.skipWhile (not << pred) x
+               grouping (List.append acc [firstGroup]) firstSkip
+    grouping [] l
+    |> List.filter (fun l -> l <> [])
+    
+
+let partB adapters =
+    let withExtra = List.append [0L;(List.max adapters) + 3L] adapters
+    let sequences = List.sort withExtra
+                    |> List.pairwise
+                    |> List.map (fun (x,y) -> y - x)
+                    |> List.map int
+                    
+    printfn "Sequences: %A" sequences                
+                    
+    let permutes n = match n with
+                     | 2 -> 2L
+                     | 3 -> 4L
+                     | 4 -> 7L
+                     | _ -> 1L
+                     
+    let rec groupedSequences = groupsPassingPredicate (fun i -> i = 1) sequences
+    
+    printfn "Grouped sequences: %A" groupedSequences
+        
+    groupedSequences
+    |> List.map List.length
+    |> List.map permutes
+    |> List.reduce (*)
+    
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllLines("Z:/aoc/day10")
+                |> Array.map Int64.Parse
+                |> Array.toList
+              
+    input
+    |> partA
+    |> printfn "Solution part A: %d"
+    
+    input
+    |> partB
+    |> printfn "Solution part B: %d"
+                
+    0 // return an integer exit code
---
+++ Day11/Program.fs
@@ -0,0 +1,114 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+open System.Reflection.Metadata
+
+type floorSpace =
+    | Unoccupied
+    | Occupied
+    | Floor
+
+
+let listOfListsToArray ll =
+    List.map List.toArray ll
+    |> List.toArray
+  
+
+let radicalCountOccupied (allInput : floorSpace [][]) (x,y) =
+    let (upX,upY) = (Array.length allInput,Array.length allInput.[0])
+    let allAround = [
+                     (-1,-1);(0,-1);(+1,-1)
+                     (-1, 0);        (+1,0)
+                     (-1,+1);(0,+1);(+1,+1)
+                    ]
+    let rec checkDirection (dx,dy) (px,py) =
+        let (nx,ny) = (px+dx,py+dy)
+        if (nx < 0) || (nx >= upX) || (ny < 0) || (ny >= upY) then
+            false
+        else
+            match allInput.[nx].[ny] with
+            | Unoccupied -> false
+            | Occupied -> true
+            | Floor -> checkDirection (dx,dy) (nx,ny)
+    List.filter (fun d -> checkDirection d (x,y)) allAround
+    |> List.length
+
+let countOccupied (allInput : floorSpace [] []) (x,y) =
+    let allAround = [
+                     (x-1,y-1);(x,y-1);(x+1,y-1)
+                     (x-1,  y);        (x+1,  y)
+                     (x-1,y+1);(x,y+1);(x+1,y+1)
+                    ]
+    List.filter (fun (cx,cy) -> let square = allInput.[cx].[cy]
+                                square = Occupied) allAround
+    |> List.length
+    
+let applyRules (state: floorSpace [] []) space (x,y) =
+    let currently = space
+    let around = countOccupied state (x,y)
+    match currently with
+    | Unoccupied -> if around = 0 then Occupied else Unoccupied
+    | Occupied -> if around >= 4 then Unoccupied else Occupied
+    | x -> x
+    
+let applyRadicalRules (state: floorSpace [] []) space (x,y) =
+    let currently = space
+    let around = radicalCountOccupied state (x,y)
+    match currently with
+    | Unoccupied -> if around = 0 then Occupied else Unoccupied
+    | Occupied -> if around >= 5 then Unoccupied else Occupied
+    | x -> x    
+ 
+let rec runSim ruleApplier (initial : floorSpace [][]) =
+    let newState = Array.mapi (fun x row -> Array.mapi (fun y space -> 
+                                                                       if space <> Floor then
+                                                                            ruleApplier initial space (x,y)
+                                                                       else
+                                                                            space) row ) initial
+    if newState = initial then
+        newState
+    else
+        runSim ruleApplier newState
+      
+let mapper c =
+    match c with
+    | 'L' -> Unoccupied
+    | '#' -> Occupied
+    | '.' -> Floor
+
+let inputToLists input =
+    Array.map (fun s -> (Seq.toList s) |> List.map mapper) input
+    |> Array.toList
+    
+let padInput input =
+    let firstRowReference = List.head input
+    let headerRow = List.replicate (List.length firstRowReference + 2) Floor
+    let widened = List.map (fun row -> List.append (Floor::row) ([Floor])) input
+    List.append (headerRow::widened) ([headerRow])
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllLines("Z:/aoc/day11")
+    
+    let floored = input
+                  |> inputToLists
+                  //|> padInput
+
+    
+    
+    printfn "Floored:\n %A" floored
+    
+    let stable = floored
+                 |> listOfListsToArray
+                 |> runSim applyRadicalRules
+    
+    stable
+    |> Array.map (fun row -> Array.filter (fun space -> space = Occupied) row)
+    |> Array.concat
+    |> Array.length
+    |> printfn "%d occupied seats"
+    
+    //printfn "Stable:\n %A" stable
+    
+    0 // return an integer exit code
